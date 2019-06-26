a0156a76ee66ca38121ee632846d8b5cd694235f
Josh Hemsley <josh@breathehr.com>
2019-06-26 14:19:09 +0100

    Updated Files from earlier work

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,18 @@
+require 'pry'
+class Birthday
 
+  def initialize
+    @list = []
+  end
+
+  def store(name, birthday)
+    @list << { name: name, birthday: birthday }
+  end
+
+  def print_all
+      @list.each do |bday|
+        puts "#{bday[:name]}: #{bday[:birthday]}"
+      end
+    end
+# binding.pry
+end
